60ceeef9feaa088026b09cfdf0f4f65be3baf601
SamahDiab <samdiab70@gmail.com>
2019-09-15 13:04:02 +0200

    adding health and motherhood pages

---
+++ js/motherhood.js
@@ -0,0 +1,34 @@
+
+
+$(document).ready(function(){
+    $("#loading").fadeOut(500);
+   
+ 
+})
+
+$(window).scroll(function(){
+    let scrollTop = $(window).scrollTop();
+    
+    if(scrollTop > 900){
+        $("#btnUp").fadeIn(500);
+    }
+    else{
+        $("#btnUp").fadeOut(500);
+
+    }
+    
+})
+$("#btnUp").click(function(){
+    
+    $("body,html").animate({scrollTop:0},1000);
+})
+
+$("a").click(function(){
+    let aHref = $(this).attr("href");
+    let itemOffset = $(aHref).offset().top;
+    
+    $("body,html").animate({scrollTop:itemOffset},1000);
+    
+})
+
+
